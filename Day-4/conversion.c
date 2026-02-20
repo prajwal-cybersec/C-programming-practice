280e8fec91fcbd1563937f25eb7130a198c5671d
prajwal-cybersec <prajwalbn2007@gmail.com>
2026-02-20 22:06:12 +0530

    Add conversion.c for type conversion example

---
+++ Day-4/conversion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    int b = 2;
+
+    float sum;
+
+    sum = (float)a + b;   // type conversion
+
+    printf("Sum = %.2f\n", sum);
+
+    return 0;
+}
