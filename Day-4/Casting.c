66e86c3ba6c352048d679526c0e1b4675cd7e736
prajwal-cybersec <prajwalbn2007@gmail.com>
2026-02-20 22:09:17 +0530

    Add casting example in Casting.c
    Implement basic integer to float casting in division.

---
+++ Day-4/Casting.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int b = 3;
+
+    float result;
+
+    result = (float)a / b;   // casting int to float
+
+    printf("Result = %.2f\n", result);
+
+    return 0;
+}
